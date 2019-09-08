9f5f8c6a2bc836d6a0a3fb84a152717d8477bd19
Eric Biondic <biondiceric@gmail.com>
2019-09-08 09:55:58 -0600

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Helena Goobar',
+        username: 'helgoobs',
+        email: 'helloandgoobye@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Barn Cromwell',
+        username: 'tangito',
+        email: 'lifesabarn@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Franco Boullabaise',
+        username: 'WretchedStew',
+        email: 'lonegumball@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Passport Unknown',
+        username: 'stong-wengchek',
+        email: 'passportunk@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Leslie Zeal',
+        username: 'Branch-Lunch-Crunch',
+        email: 'zeal_le_greek@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Belena Hoobar',
+        username: 'helgoobs',
+        email: 'helloandgoobye@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Farm Cromwell',
+        username: 'tangito',
+        email: 'lifesabarn@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Cranko Ghoullabaise',
+        username: 'WretchedStew',
+        email: 'lonegumball@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Classport Unknown',
+        username: 'stong-wengchek',
+        email: 'passportunk@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Less-Lee Seal',
+        username: 'Branch-Lunch-Crunch',
+        email: 'zeal_le_greek@gmail.com'
+    }
+]
